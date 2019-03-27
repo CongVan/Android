931c5b8903e515b680930f3a758336164dedbecc
VanHuynh <congvan203@gmail.com>
2019-03-27 15:56:57 +0700

    change place of bottom sheet main to fragment

--- MusicForLife/app/src/main/java/com/example/musicforlife/FragmentListSong.java
+++ MusicForLife/app/src/main/java/com/example/musicforlife/FragmentListSong.java
@@ -8,15 +8,21 @@ import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -26,10 +32,18 @@ import java.util.List;
 public class FragmentListSong extends Fragment {
     MainActivity _mainActivity;
     Context _context;
+    LayoutInflater _inflater;
     ArrayList<SongModel> _listSong;
-    LinearLayout _layoutListSong;
+    CoordinatorLayout _layoutListSong;
     ListView _listViewSong;
     ListSongAdapter _listSongAdapter;
+    Button btnCheckout;
+    Button btnBottomSheet;
+    LinearLayout layoutBottomSheet;
+
+    BottomSheetBehavior sheetBehavior;
+
+    private static final String TAG = "FragmentListSong";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,10 +66,20 @@ public class FragmentListSong extends Fragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: STARTED");
+        new loadImageFromStorage().execute();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        _listSong = getAllAudioFromDevice(_context);
-        _layoutListSong = (LinearLayout) inflater.inflate(R.layout.layout_list_song, null);
-        _listViewSong = (ListView) _layoutListSong.findViewById(R.id.lsvSongs);
+        Log.i(TAG, "onCreateView: STARTED CREATE VIEW");
+
+        _listSong = new ArrayList<>();// getAllAudioFromDevice(_context);
+        _inflater = inflater;
+        _layoutListSong = (CoordinatorLayout) _inflater.inflate(R.layout.layout_list_song, null);
+        _listViewSong = _layoutListSong.findViewById(R.id.lsvSongs);
         _listSongAdapter = new ListSongAdapter(_context, _listSong);
         _listViewSong.setAdapter(_listSongAdapter);
         _listViewSong.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -65,20 +89,71 @@ public class FragmentListSong extends Fragment {
             }
         });
 
+        btnCheckout = _layoutListSong.findViewById(R.id.btnCheckout);
+        btnCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(_context, "OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnBottomSheet = _layoutListSong.findViewById(R.id.btn_bottom_sheet);
+        layoutBottomSheet = _layoutListSong.findViewById(R.id.bottom_sheet);
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+//        sheetBehavior.setPeekHeight(BottomSheetBehavior.PEEK_HEIGHT_AUTO);
+//        layoutBottomSheet.getLayoutParams().height = ViewGroup.LayoutParams.MATCH_PARENT;
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        btnBottomSheet.setText("Close Sheet");
+//                        bottomNavigationView.setSystemUiVisibility(View.INVISIBLE);
+                        layoutBottomSheet.bringToFront();
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        btnBottomSheet.setText("Expand Sheet");
+//                        bottomNavigationView.setSystemUiVisibility(View.VISIBLE);
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                }
+            }
 
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
         return _layoutListSong;
+    }
+
+    public void toggleBottomSheet(View view) {
+        if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            btnBottomSheet.setText("Close sheet");
+        } else {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            btnBottomSheet.setText("Expand sheet");
+        }
     }
 
     public ArrayList<SongModel> getAllAudioFromDevice(final Context context) {
         final ArrayList<SongModel> tempAudioList = new ArrayList<SongModel>();
-
         Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
 //        String[] projection = {MediaStore.Audio.AudioColumns.DATA, MediaStore.Audio.AudioColumns.TITLE, MediaStore.Audio.AudioColumns.ALBUM, MediaStore.Audio.ArtistColumns.ARTIST,};
         Cursor c = context.getContentResolver().query(uri, null, null, null, null);
-        int debugLoop=20;
+        int debugLoop = 20;
         if (c != null) {
-            int count=0;
-            while (c.moveToNext() && count++<debugLoop) {
+            int count = 0;
+            while (c.moveToNext()) {// && count++<debugLoop
+                count++;
+                Log.d(TAG, "getAllAudioFromDevice: " + count);
                 SongModel songModel = new SongModel();
                 String path = c.getString(c.getColumnIndex(MediaStore.Audio.AudioColumns.DATA));
                 String name = c.getString(c.getColumnIndex(MediaStore.Audio.AudioColumns.TITLE));
@@ -90,17 +165,15 @@ public class FragmentListSong extends Fragment {
                 mediaMetadataRetriever.setDataSource(path);
                 InputStream inputStream = null;
                 Bitmap bitmap = null;
-                try {
-                    inputStream = new ByteArrayInputStream(mediaMetadataRetriever.getEmbeddedPicture());
-                    mediaMetadataRetriever.release();   
-                    bitmap = BitmapFactory.decodeStream(inputStream);
-                } catch (Exception ex) {
-                    bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.musical_note_light_64);
-                }
 
-//                if (mediaMetadataRetriever.getEmbeddedPicture() != null) {
-//
-//                }
+
+                if (mediaMetadataRetriever.getEmbeddedPicture() != null) {
+                    inputStream = new ByteArrayInputStream(mediaMetadataRetriever.getEmbeddedPicture());
+                    mediaMetadataRetriever.release();
+                    bitmap = BitmapFactory.decodeStream(inputStream);
+                } else {
+                    bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.musical_note_light_64);
+                }
 
 
                 songModel.setTitle(name);
@@ -116,9 +189,9 @@ public class FragmentListSong extends Fragment {
             }
             c.close();
         }
-
         return tempAudioList;
     }
+
     public static String formateMilliSeccond(long milliseconds) {
         String finalTimerString = "";
         String secondsString = "";
@@ -150,4 +223,81 @@ public class FragmentListSong extends Fragment {
         // return timer string
         return finalTimerString;
     }
+
+    private class loadImageFromStorage extends AsyncTask<Void, Integer, ArrayList<SongModel>> {
+
+        @Override
+        protected void onPostExecute(ArrayList<SongModel> songModels) {
+            super.onPostExecute(songModels);
+            _listSong.addAll(songModels);
+            Log.i(TAG, "onPostExecute: SONGS--> " + _listSong.size());
+            _listViewSong.post(new Runnable() {
+                @Override
+                public void run() {
+                    _listSongAdapter.notifyDataSetChanged();
+                }
+            });
+            Log.i(TAG, "onPostExecute: FINISHED");
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        public ArrayList<SongModel> doInBackground(Void... voids) {
+            final ArrayList<SongModel> tempAudioList = new ArrayList<SongModel>();
+            Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+//        String[] projection = {MediaStore.Audio.AudioColumns.DATA, MediaStore.Audio.AudioColumns.TITLE, MediaStore.Audio.AudioColumns.ALBUM, MediaStore.Audio.ArtistColumns.ARTIST,};
+            Cursor c = _context.getContentResolver().query(uri, null, null, null, null);
+            int debugLoop = 40;
+            if (c != null) {
+                int count = 0;
+                while (c.moveToNext() && count++ < debugLoop) {// && count++<debugLoop
+                    SongModel songModel = new SongModel();
+                    String path = c.getString(c.getColumnIndex(MediaStore.Audio.AudioColumns.DATA));
+                    String name = c.getString(c.getColumnIndex(MediaStore.Audio.AudioColumns.TITLE));
+                    String album = c.getString(c.getColumnIndex(MediaStore.Audio.AudioColumns.ALBUM));
+                    String artist = c.getString(c.getColumnIndex(MediaStore.Audio.AudioColumns.ARTIST));
+                    String duration = c.getString(c.getColumnIndex(MediaStore.Audio.AudioColumns.DURATION));
+
+                    MediaMetadataRetriever mediaMetadataRetriever = new MediaMetadataRetriever();
+                    mediaMetadataRetriever.setDataSource(path);
+                    InputStream inputStream = null;
+                    Bitmap bitmap = null;
+//                    try {
+//
+//                    } catch (Exception ex) {
+//
+//                    }
+
+                    if (mediaMetadataRetriever.getEmbeddedPicture() != null) {
+                        inputStream = new ByteArrayInputStream(mediaMetadataRetriever.getEmbeddedPicture());
+                        mediaMetadataRetriever.release();
+                        bitmap = BitmapFactory.decodeStream(inputStream);
+                    } else {
+                        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.musical_note_light_64);
+                    }
+
+
+                    songModel.setTitle(name);
+                    songModel.setAlbum(album);
+                    songModel.setArtist(artist);
+                    songModel.setPath(path);
+                    songModel.setBitmap(bitmap);
+                    songModel.setDuration(formateMilliSeccond(Long.valueOf(duration)));
+//                Log.e("Name :" + name, " Album :" + album);
+//                Log.e("Path :" + path, " Artist :" + artist);
+
+                    tempAudioList.add(songModel);
+                }
+                c.close();
+            }
+            return tempAudioList;
+        }
+
+
+    }
+
 }
--- MusicForLife/app/src/main/java/com/example/musicforlife/MainActivity.java
+++ MusicForLife/app/src/main/java/com/example/musicforlife/MainActivity.java
@@ -1,24 +1,53 @@
 package com.example.musicforlife;
+
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.widget.SearchView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+//import butterknife.BindView;
+//import butterknife.ButterKnife;
+//import butterknife.OnClick;
 
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     FragmentListSong fragmentListSong;
+    FrameLayout frameLayoutContainer = null;
+    Handler _mainHandle = new Handler();
+    FragmentThread fragmentThread = new FragmentThread();
+    //    @BindView(R.id.btn_bottom_sheet)
+    Button btnBottomSheet;
+
+    //    @BindView(R.id.bottom_sheet)
+    LinearLayout layoutBottomSheet;
+
+    BottomSheetBehavior sheetBehavior;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadFragment();
+//        ButterKnife.bind(this);
+        frameLayoutContainer = findViewById(R.id.frame_container);
+//        btnBottomSheet = findViewById(R.id.btn_bottom_sheet);
+//        layoutBottomSheet = findViewById(R.id.bottom_sheet);
+//        fragmentThread.run();
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar_main);
 //
 //
@@ -26,24 +55,25 @@ public class MainActivity extends AppCompatActivity  {
 //        // Make sure the toolbar exists in the activity and is not null
 //        setSupportActionBar(toolbar);
 //
-        BottomNavigationView bottomNavigationView=findViewById(R.id.navigation_main);
+
+//        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+        final BottomNavigationView bottomNavigationView = findViewById(R.id.navigation_main);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.navigation_song:{
-                        Toast.makeText(MainActivity.this,"HOME",Toast.LENGTH_SHORT).show();
-
-                        loadFragment();
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_song: {
+//                        Toast.makeText(MainActivity.this, "HOME", Toast.LENGTH_SHORT).show();
+                        fragmentThread.run();
                         break;
                     }
 //                    case  R.id.navigation_song:{
 //                        Toast.makeText(MainActivity.this,"SONG",Toast.LENGTH_SHORT).show();
 //                        break;
 //                    }
-                    case  R.id.navigation_album:{
-                        Toast.makeText(MainActivity.this,"ALBUM",Toast.LENGTH_SHORT).show();
+                    case R.id.navigation_album: {
+//                        Toast.makeText(MainActivity.this, "ALBUM", Toast.LENGTH_SHORT).show();
                         break;
                     }
                 }
@@ -51,14 +81,82 @@ public class MainActivity extends AppCompatActivity  {
                 return true;
             }
         });
-    }
-    private  void loadFragment(){
-        //load fragment
-        FragmentTransaction transaction= getFragmentManager().beginTransaction();
-        fragmentListSong=FragmentListSong.newInstance();
-        transaction.replace(R.id.frame_container,fragmentListSong);
-        transaction.commit();
+/**
+ * bottom sheet state change listener
+ * we are changing button text when sheet changed state
+ * */
+//        layoutBottomSheet.getLayoutParams().height = ViewGroup.LayoutParams.MATCH_PARENT;
+//        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+//            @Override
+//            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+//                switch (newState) {
+//                    case BottomSheetBehavior.STATE_HIDDEN:
+//                        break;
+//                    case BottomSheetBehavior.STATE_EXPANDED: {
+//                        btnBottomSheet.setText("Close Sheet");
+////                        bottomNavigationView.setSystemUiVisibility(View.INVISIBLE);
+//                        layoutBottomSheet.bringToFront();
+//                    }
+//                    break;
+//                    case BottomSheetBehavior.STATE_COLLAPSED: {
+//                        btnBottomSheet.setText("Expand Sheet");
+////                        bottomNavigationView.setSystemUiVisibility(View.VISIBLE);
+//                    }
+//                    break;
+//                    case BottomSheetBehavior.STATE_DRAGGING:
+//                        break;
+//                    case BottomSheetBehavior.STATE_SETTLING:
+//                        break;
+//                }
+//            }
+//
+//            @Override
+//            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+//
+//            }
+//        });
+
     }
 
+    /**
+     * manually opening / closing bottom sheet on button click
+     */
+
+    public void toggleBottomSheet(View view) {
+        if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            btnBottomSheet.setText("Close sheet");
+        } else {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            btnBottomSheet.setText("Expand sheet");
+        }
+    }
+
+    private void loadFragment() {
+        //load fragment
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        fragmentListSong = FragmentListSong.newInstance();
+        transaction.replace(R.id.frame_container, fragmentListSong);
+        transaction.commit();
+
+    }
+
+    private class FragmentThread extends Thread {
+        @Override
+        public void run() {
+
+
+            Handler threadHandle = new Handler(Looper.getMainLooper());
+            threadHandle.post(new Runnable() {
+                @Override
+                public void run() {
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                    fragmentListSong = FragmentListSong.newInstance();
+                    transaction.replace(R.id.frame_container, fragmentListSong);
+                    transaction.commit();
+                }
+            });
+        }
+    }
 
 }
