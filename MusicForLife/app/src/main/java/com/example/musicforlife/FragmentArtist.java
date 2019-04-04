a7da83294dd26121f93ccf7a85a1126f716e0c10
giangtlq <gtran8396@github.com>
2019-04-04 23:33:26 +0700

    replace ListView to RecyclerView

--- MusicForLife/app/src/main/java/com/example/musicforlife/FragmentArtist.java
+++ MusicForLife/app/src/main/java/com/example/musicforlife/FragmentArtist.java
@@ -4,6 +4,8 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,15 +19,24 @@ public class FragmentArtist extends Fragment {
 
     View view;
     ArrayList<ArtistModel> arrArtist;
-    ListView LVArtist;
+    RecyclerView LVArtist;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //get view from infalter
         view = inflater.inflate(R.layout.fragment_artist,container,false);
-        LVArtist = (ListView) view.findViewById(R.id.lvArtistList);
+
+        //get recyclerview
+        LVArtist = (RecyclerView) view.findViewById(R.id.lvArtistList);
+
+        //get list artist from db
         arrArtist = ArtistModel.getArtistModel(getActivity());
+
+        //map layout with adapter
         ListArtistAdapter listArtistAdapter = new ListArtistAdapter(getActivity(),arrArtist);
+        LVArtist.setLayoutManager(new LinearLayoutManager(getActivity()));
         LVArtist.setAdapter(listArtistAdapter);
+
         return view;
     }
 
--- MusicForLife/app/src/main/java/com/example/musicforlife/ListArtistAdapter.java
+++ MusicForLife/app/src/main/java/com/example/musicforlife/ListArtistAdapter.java
@@ -1,6 +1,8 @@
 package com.example.musicforlife;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,39 +14,43 @@ import org.w3c.dom.Text;
 
 import java.util.List;
 
-public class ListArtistAdapter extends BaseAdapter {
+public class ListArtistAdapter extends RecyclerView.Adapter<ListArtistAdapter.ArtistViewHolder> {
     private Context myContext;
     private List<ArtistModel> artistList;
     public ListArtistAdapter(Context context,List<ArtistModel> list){
         myContext = context;
         artistList = list;
     }
+
+    @NonNull
     @Override
-    public int getCount() {
+    public ArtistViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_item_artist, viewGroup, false);
+        ArtistViewHolder artistViewHolder = new ArtistViewHolder(view);
+        return artistViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ArtistViewHolder artistViewHolder, int i) {
+        ArtistModel _aArtistModel = artistList.get(i);
+        artistViewHolder.BindData(_aArtistModel);
+    }
+    @Override
+    public int getItemCount() {
         return artistList.size();
     }
 
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater)myContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        convertView = inflater.inflate(R.layout.layout_item_artist,null);
-
-        TextView TVArtistName = (TextView)convertView.findViewById(R.id.txtArtistName);
-        TextView TVArtistCount = (TextView)convertView.findViewById(R.id.txtArtistCount);
-
-        TVArtistName.setText(artistList.get(position).getName());
-        TVArtistCount.setText(artistList.get(position).getSongCount() + "");
-
-        return convertView;
+    public static class ArtistViewHolder extends RecyclerView.ViewHolder{
+        TextView TVArtistName;
+        TextView TVArtistCount;
+        public ArtistViewHolder(@NonNull View itemView) {
+            super(itemView);
+            TVArtistName = (TextView)itemView.findViewById(R.id.txtArtistName);
+            TVArtistCount = (TextView)itemView.findViewById(R.id.txtArtistCount);
+        }
+        public void BindData(ArtistModel artistModel){
+            TVArtistName.setText(artistModel.getName());
+            TVArtistCount.setText(artistModel.getSongCount() + " Bài hát");
+        }
     }
 }
