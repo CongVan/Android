9a15d78dcb40de7e917686c84feb552a5fb5dadf
CongVan <congvan203@gmail.com>
2019-04-24 20:52:47 +0700

    fix layout tool bar, edit search view

--- MusicForLife/app/src/main/java/com/example/musicforlife/utilitys/Utility.java
+++ MusicForLife/app/src/main/java/com/example/musicforlife/utilitys/Utility.java
@@ -18,4 +18,13 @@ public class Utility {
             window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
     }
+    public static int getStatusbarHeight(Activity activity){
+        // status bar height
+        int statusBarHeight = 0;
+        int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = activity.getResources().getDimensionPixelSize(resourceId);
+        }
+        return  statusBarHeight;
+    }
 }
